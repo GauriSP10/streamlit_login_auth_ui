729f23ce2ea354bf383044c1344dd1bbfad264d3
fsmosca <fsmosca@users.noreply.github.com>
2022-12-13 00:18:49 +0800

    Refactor sign_up_widget()

--- streamlit_login_auth_ui/widgets.py
+++ streamlit_login_auth_ui/widgets.py
@@ -132,43 +132,47 @@ class __login__:
         Creates the sign-up widget and stores the user info in a secure way in the _secret_auth_.json file.
         """
         with st.form("Sign Up Form"):
-            name_sign_up = st.text_input("Name *", placeholder = 'Please enter your name')
+            name_sign_up = st.text_input("Name *", placeholder='Please enter your name')
             valid_name_check = check_valid_name(name_sign_up)
 
-            email_sign_up = st.text_input("Email *", placeholder = 'Please enter your email')
+            email_sign_up = st.text_input("Email *", placeholder='Please enter your email')
             valid_email_check = check_valid_email(email_sign_up)
             unique_email_check = check_unique_email(email_sign_up)
             
-            username_sign_up = st.text_input("Username *", placeholder = 'Enter a unique username')
+            username_sign_up = st.text_input("Username *", placeholder='Enter a unique username')
             unique_username_check = check_unique_usr(username_sign_up)
 
-            password_sign_up = st.text_input("Password *", placeholder = 'Create a strong password', type = 'password')
+            password_sign_up = st.text_input("Password *", placeholder='Create a strong password', type='password')
 
             st.markdown("###")
-            sign_up_submit_button = st.form_submit_button(label = 'Register')
+            sign_up_submit_button = st.form_submit_button(label='Register')
 
             if sign_up_submit_button:
-                if valid_name_check == False:
+                is_registration_ok = True
+
+                if not valid_name_check:
                     st.error("Please enter a valid name!")
+                    is_registration_ok = False
 
-                elif valid_email_check == False:
+                elif not valid_email_check:
                     st.error("Please enter a valid Email!")
+                    is_registration_ok = False
                 
-                elif unique_email_check == False:
+                elif not unique_email_check:
                     st.error("Email already exists!")
+                    is_registration_ok = False
                 
-                elif unique_username_check == False:
-                    st.error(f'Sorry, username {username_sign_up} already exists!')
-                
-                elif unique_username_check == None:
+                elif unique_username_check is None:
                     st.error('Please enter a non - empty Username!')
+                    is_registration_ok = False
 
-                if valid_name_check == True:
-                    if valid_email_check == True:
-                        if unique_email_check == True:
-                            if unique_username_check == True:
-                                register_new_usr(name_sign_up, email_sign_up, username_sign_up, password_sign_up)
-                                st.success("Registration Successful!")
+                elif not unique_username_check:
+                    st.error(f'Sorry, username {username_sign_up} already exists!')
+                    is_registration_ok = False
+
+                if is_registration_ok:
+                    register_new_usr(name_sign_up, email_sign_up, username_sign_up, password_sign_up)
+                    st.success("Registration Successful!")
 
 
     def forgot_password(self) -> None:

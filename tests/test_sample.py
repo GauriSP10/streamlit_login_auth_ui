ab7be2b19a271ce0e2c0e4d4c0d22291f3ff66b9
fsmosca <fsmosca@users.noreply.github.com>
2022-12-13 10:36:33 +0800

    Add pytest on check_usr_pass()

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,4 @@
-from streamlit_login_auth_ui.utils import is_empty, check_unique_usr
+from streamlit_login_auth_ui.utils import is_empty, check_unique_usr, check_usr_pass
 
 
 def test_empty_username_1(username=' '):
@@ -26,3 +26,26 @@ def test_unique_username_2(username='', users_info='./tests/users.json'):
 def test_unique_username_3(username='peter', users_info='./tests/users.json'):
     """Returns True"""
     assert check_unique_usr(username, users_info)
+
+
+def test_check_usr_pass_1(username='smith', password='door_5954', users_info='./tests/users.json'):
+    """Returns True"""
+    assert check_usr_pass(username, password, users_info)
+
+
+def test_check_usr_pass_2(username='Smith', password='door_5954', users_info='./tests/users.json'):
+    """Checks if username and password is in the users.json.
+
+    It returns True because username check is case insensitive. smith is the one 
+    registered in users.json. The one we are checking is Smith with capital S.
+    smith and Smith are the same person.
+    """
+    assert check_usr_pass(username, password, users_info)
+
+
+def test_check_usr_pass_3(username='Will', password='door_5954', users_info='./tests/users.json'):
+    """Checks if username and password is in the users.json.
+
+    It returns False because Will username does not exist in users.json.    
+    """
+    assert not check_usr_pass(username, password, users_info)

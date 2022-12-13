bd4117238016a79c44bde4f11ab2421534f33725
fsmosca <fsmosca@users.noreply.github.com>
2022-12-13 16:39:12 +0800

    Refactor test_sample.py as is_empty() was removed

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,4 @@
-from streamlit_login_auth_ui.utils import is_empty, check_unique_usr, check_usr_pass
-
-
-def test_empty_username_1(username=' '):
-    assert is_empty(username)
-
-
-def test_empty_username_2(username='  '):
-    assert is_empty(username)
-
-
-def test_empty_username_3(username='john'):
-    assert not is_empty(username)
+from streamlit_login_auth_ui.utils import check_unique_usr, check_usr_pass
 
 
 def test_unique_username_1(username='smith', users_info='./tests/users.json'):
@@ -18,12 +6,7 @@ def test_unique_username_1(username='smith', users_info='./tests/users.json'):
     assert not check_unique_usr(username, users_info)
 
 
-def test_unique_username_2(username='', users_info='./tests/users.json'):
-    """Returns None"""
-    assert check_unique_usr(username, users_info) is None
-
-
-def test_unique_username_3(username='peter', users_info='./tests/users.json'):
+def test_unique_username_2(username='peter', users_info='./tests/users.json'):
     """Returns True"""
     assert check_unique_usr(username, users_info)
 
